049e5d7d29c508aa307d6d667951a9cb2746213b
Scxrpion2k <wilmerbladimir11@gmail.com>
2026-06-26 22:03:42 -0400

    Creando las aplicaciones

---
+++ practica-4/Aplicacion2/src/main/java/edu/pucmm/eict/Main.java
@@ -0,0 +1,20 @@
+package edu.pucmm.eict;
+
+
+import io.javalin.Javalin;
+
+public class Main {
+    static void main(String[] args) {
+        Javalin app = Javalin.create(config ->{
+
+            config.routes.get("/",ctx -> {
+                ctx.result("Prueba");
+            });
+
+
+        });
+
+        app.start(7001);
+
+    }
+}
